794aa590a1f672d5c6a64381def958f8fb96b20d
BPitts8019 <bradleyj.pitts@gmail.com>
2019-07-11 16:39:22 -0700

    Added Person class to Lambda School roster

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
-// CODE here for your Lambda Classes
+// const fred = new Instructor({
+//    name: 'Fred',
+//    location: 'Bedrock',
+//    age: 37,
+//    favLanguage: 'JavaScript',
+//    specialty: 'Front-end',
+//    catchPhrase: `Don't forget the homies`
+//  });
+
+/**
+ * Base Class for Lambda School Roster
+ * @param {Object} props - An object with the following keys:
+ * name: Person's name
+ * age: Person's age in years
+ * location: Where the person lives
+ */
+class Person {
+   constructor ({name, age, location}) {
+      this.name = name;
+      this.age = age;
+      this.location = location;
+   }
+
+   speak () {
+      console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+   }
+}
