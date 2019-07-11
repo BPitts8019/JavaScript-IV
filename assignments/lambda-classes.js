60bb0fe660d17e075e1c629544c300bbc3ea251a
BPitts8019 <bradleyj.pitts@gmail.com>
2019-07-11 16:48:08 -0700

    Added Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,3 +25,37 @@ class Person {
       console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
    }
 }
+
+/**
+ * Instructor Class
+ * @param {Object} props - An object with the following keys:
+ * name: Person's name
+ * age: Person's age in years
+ * location: Where the person lives
+ */
+class Instructor extends Person {
+   constructor ({specialty, favLanguage, catchPhrase, ...rest}) {
+      super(rest);
+      this.specialty = specialty;
+      this.favLanguage = favLanguage;
+      this.catchPhrase = catchPhrase;
+   }
+
+   /**
+    * A simple speech bubble for the instructor
+    * @param {string} subject 
+    */
+   demo (subject) {
+      console.log(`Today we are learning about ${subject}`);
+   }
+
+   /**
+    * Another speech bubble for the instructor
+    * @param {Object} student 
+    * @param {string} subject 
+    */
+   grade (student, subject) {
+      console.log(`${student.name} receives a perfect score on ${subject}`);
+   }
+}
+
